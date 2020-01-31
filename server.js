c6db6c0c5d74b8965152a1b331736717234da02b
CAGJ99 <cegjofre@gmail.com>
2020-01-31 20:21:49 -0300

    Server editado

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/weather-info'));
+app.use(express.static(__dirname + '/dist/climamax'));
 
 app.get('/*', function(req, res) {
 
-    res.sendFile(path.join(__dirname + '/dist/weather-info/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/climamax/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
